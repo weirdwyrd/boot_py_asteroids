c2ff53e0f42331a0ba7c720d6e9f2dbd3bae0abb
samual <samual.j.thor@gmail.com>
2025-01-12 11:06:43 -0600

    asteroid field

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from constants import (
     ASTEROID_MAX_RADIUS,
 )
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -22,9 +24,13 @@ def main():
     # grouping
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
     # characters
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
     # delta
     dt = 0
 
